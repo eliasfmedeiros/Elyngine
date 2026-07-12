ebb48d42d9470d7baa3aeed71c2ff9dfa9594a09
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-07-12 04:01:19 -0300

    feat: add essential manipulation operations to UniDeque class
    Implements the core public operations of UniDeque.

--- src/internal/UniDeque.java
+++ src/internal/UniDeque.java
@@ -7,7 +7,7 @@ public class UniDeque<T> implements Collection<T> {
 
 	private StackerNode<T> lastNode=null; // Implicit CONSTRUCTOR initialization
 	
-    //#region basic
+    //#region internal infrastructure
 	public boolean isEmpty() {
 		return lastNode==null;
 	}
@@ -44,11 +44,41 @@ public class UniDeque<T> implements Collection<T> {
 	}
     //#endregion
 
-	@Override
+    //#region basic
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		if(this.isEmpty()) return 0;
+		int i=1;
+		for(StackerNode<T> node=getFirstNode();node!=lastNode;node=(StackerNode<T>) node.getNext()) i++;
+		return i;
 	}
+
+    public T getFirst() {
+        return this.isEmpty()?null:this.getFirstNode().getContent();
+    }
+
+	public T getLast() { 
+        return this.isEmpty()?null:lastNode.getContent();
+	}
+    	
+	public void addFirst(T content) {
+		this.addFirstNode(new UniNode<>(content));
+	}
+
+	public void addLast(T content) {
+		this.addFirst(content);
+		this.moveFirstNodeToLast();
+	}
+	
+	public T removeFirst() {
+		return this.forgetFirstNode().getContent();
+	}
+	
+	public StackerNode<T> removeFirstNode() {
+		StackerNode<T> removed=this.forgetFirstNode();
+		removed.setNext(removed);
+		return removed;
+	}
+    //#endregion
 
 	@Override
 	public boolean contains(Object o) {
